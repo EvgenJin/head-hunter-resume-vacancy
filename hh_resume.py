0c5608a308a3db4000e8d58f6168c446a9ad547a
EvgenJin <rumbiroid@gmail.com>
2020-01-14 14:54:43 +0500

    s

--- hh_resume.py
+++ hh_resume.py
@@ -6,8 +6,8 @@ import pandas as pd
 # страница со всеми резюме 
 def get_all (page):
   period = 7 # за сколько дней
-  area = 1 # Екатеринбург 3 / Москва 1
-  querry = 'plsql' # Ключевое слово
+  area = 3 # Екатеринбург 3 / Москва 1
+  querry = 'java' # Ключевое слово
   url = 'https://hh.ru/search/resume?text='+querry +'&specialization=1&clusters=true&order_by=publication_time&no_magic=false&st=resumeSearch&logic=normal&pos=keywords&order_by=publication_time&exp_period=all_time&area=' + str(area) + '&search_period='+str(period)+'&page='+str(page)
   #pos=full_text / keywords
   headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
@@ -17,7 +17,7 @@ def get_all (page):
   # print(res.status_code)
   return data_all
 
-# scan page with one vacancy
+# страница с одним резюме 
 def get_one(id):
   url = 'https://hh.ru'+id
   headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
@@ -40,25 +40,31 @@ i=0
 # запрашивать пока массив не опустеет
 while len(get_all(i)) > 0:
   result_all = get_all(i)
-  # i = i + 1
   i +=1
   for item in result_all:
     id = item.find('a', href=True)['href']
     result_one = get_one(id)
     soup_one = BeautifulSoup(result_one.content, 'html.parser')
+    # наименование
     title = soup_one.find('span',class_='resume-block__title-text resume-block__title-text_position').find("span")
+    # дата рождения
     dbirth = soup_one.find('meta',itemprop="birthDate")
     if dbirth:
       dbirth = dbirth["content"]
     else:
-      dbirth = ''    
+      dbirth = ''  
+    # желаемая зп
     salary = soup_one.find('span',class_='resume-block__salary resume-block__title-text_salary')
+    # последнее место работы
     last_work = soup_one.find('div',class_='resume-block__sub-title')
+    # последнее место работы период
     last_work_per = soup_one.find('div',class_='bloko-column bloko-column_xs-4 bloko-column_s-2 bloko-column_m-2 bloko-column_l-2')
+    # строка для dataframe
     row = [if_null_bf(title),dbirth,if_null_bf(salary),if_null_bf(last_work),if_null_bf(last_work_per),'https://hh.ru'+id]
     rows.append(row)
-
+# затолкать в dataframe
 df = pd.DataFrame(rows, columns=columns)
+# результат
 print(df[['title','dbirth','salary','last_work']])
 
 
--- hh_vacancy.py
+++ hh_vacancy.py
@@ -5,14 +5,14 @@ import pandas as pd
 from pandas.io.json import json_normalize
 from datetime import datetime  
 from datetime import timedelta  
-
-
 import sys
+
+days = 4
 
 def get_request (page):
   # запрос
   q = str(sys.argv[1]) 
-  # регион или город (1 москва 3 екатеринбург)
+  # регион или город (1 москва, 3 екатеринбург) (https://api.hh.ru/areas)
   area = str(sys.argv[2])
   url = 'https://api.hh.ru/vacancies?area='+str(area)+'&page='+str(page)+'&per_page=10&text='+q
   res = requests.get(url)
@@ -20,7 +20,7 @@ def get_request (page):
 
 i=1
 df = pd.DataFrame()
-# идти по всем станицам пока не придет статус не 200, запихать все в один фрейм
+# идти по всем страницам пока не придет статус не 200, запихать все в один фрейм
 while get_request(i).status_code == 200 and len(get_request(i).json()['items']) > 0:
   json_data = get_request(i).json()
   norm = json_normalize(json_data['items'])
@@ -29,18 +29,18 @@ while get_request(i).status_code == 200 and len(get_request(i).json()['items']) 
   df = pd.concat([df,dfi],sort=False)
   i = i + 1
 
-# дата - 1 
-new = datetime.now() + timedelta(days=-20)
+new = datetime.now() + timedelta(days=-days)
 # указана зарплата
 with_zp = df[df['salary.from'] > 0][['name','employer.name','salary.from','salary.to','published_at','alternate_url']]
 # не указана зарплата
 without_zp = df[['name','employer.name','salary.from','salary.to','published_at','alternate_url']]
 # без зарплаты , но новые 
 without_zp_new = df[df['published_at'] >= str(new)][['name','employer.name','salary.from','salary.to','published_at','alternate_url']]
-without_zp_new.to_excel("output.xlsx")
+#print(df.agg({'salary.from':['mean','median','count']}))
+print(without_zp_new)
+# without_zp_new.to_excel("output.xlsx")
 # print(without_zp_new)
 # print(df[(df['salary.from'] > 0) & (df['salary.to'] > 0)][['name','employer.name','salary.from','salary.to','published_at','alternate_url','snippet.requirement','snippet.responsibility']])
 # print(df[df['salary.from'] > 0][['name','employer.name','salary.from','salary.to','published_at','alternate_url']])
 # print(df[['name','employer.name','salary.from','salary.to','published_at','alternate_url']])
-#print(df.agg({'salary.from':['mean','median','count']}))
 #df[df['salary.from'] > 0][['name','employer.name','salary.from','salary.to','published_at','alternate_url','snippet.requirement','snippet.responsibility']].to_csv('file.csv',encoding='utf-8-sig',sep=';')
