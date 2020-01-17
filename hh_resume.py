38b13d6e00e6b6ffe10a8bdc83303d4d398dbbd7
EvgenJin <rumbiroid@gmail.com>
2020-01-17 09:05:48 +0500

    1
    1

--- hh_resume.py
+++ hh_resume.py
@@ -1,14 +1,16 @@
+# -*- coding: UTF-8 -*-
 from bs4 import BeautifulSoup
 import requests
 import re
 import pandas as pd
 
+
 # страница со всеми резюме 
-def get_all (page):
-  period = 7 # за сколько дней
-  area = 3 # Екатеринбург 3 / Москва 1
-  querry = 'java' # Ключевое слово
-  url = 'https://hh.ru/search/resume?text='+querry +'&specialization=1&clusters=true&order_by=publication_time&no_magic=false&st=resumeSearch&logic=normal&pos=keywords&order_by=publication_time&exp_period=all_time&area=' + str(area) + '&search_period='+str(period)+'&page='+str(page)
+def get_all (query,area,period,page):
+  # period = 7 # за сколько дней
+  # area = 3 # Екатеринбург 3 / Москва 1
+  # query = 'Java' # Ключевое слово
+  url = 'https://hh.ru/search/resume?text='+query +'&specialization=1&clusters=true&order_by=publication_time&no_magic=false&st=resumeSearch&logic=normal&pos=keywords&order_by=publication_time&exp_period=all_time&area=' + str(area) + '&search_period='+str(period)+'&page='+str(page)
   #pos=full_text / keywords
   headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
   res = requests.get(url, headers=headers)
@@ -17,7 +19,7 @@ def get_all (page):
   # print(res.status_code)
   return data_all
 
-# страница с одним резюме 
+# scan page with one vacancy
 def get_one(id):
   url = 'https://hh.ru'+id
   headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
@@ -29,44 +31,45 @@ def if_null_bf(par):
   if par:
     res = par.get_text()
   else:
-    res = ''  
+    res = ''
   return res
 
-# колонки и массив строк для dataframe
-columns = ['title','dbirth','salary','last_work','last_work_per','url']
-rows = []
+def if_null_num(par):
+  if par:
+    input = re.sub(r"\s+", "", par.get_text(), flags=re.UNICODE)
+    res = re.match(r'\d+',input).group(0)
+    res = int(res)
+  else:
+    res = None
+  return res
 
-i=0
-# запрашивать пока массив не опустеет
-while len(get_all(i)) > 0:
-  result_all = get_all(i)
-  i +=1
-  for item in result_all:
-    id = item.find('a', href=True)['href']
-    result_one = get_one(id)
-    soup_one = BeautifulSoup(result_one.content, 'html.parser')
-    # наименование
-    title = soup_one.find('span',class_='resume-block__title-text resume-block__title-text_position').find("span")
-    # дата рождения
-    dbirth = soup_one.find('meta',itemprop="birthDate")
-    if dbirth:
-      dbirth = dbirth["content"]
-    else:
-      dbirth = ''  
-    # желаемая зп
-    salary = soup_one.find('span',class_='resume-block__salary resume-block__title-text_salary')
-    # последнее место работы
-    last_work = soup_one.find('div',class_='resume-block__sub-title')
-    # последнее место работы период
-    last_work_per = soup_one.find('div',class_='bloko-column bloko-column_xs-4 bloko-column_s-2 bloko-column_m-2 bloko-column_l-2')
-    # строка для dataframe
-    row = [if_null_bf(title),dbirth,if_null_bf(salary),if_null_bf(last_work),if_null_bf(last_work_per),'https://hh.ru'+id]
-    rows.append(row)
-# затолкать в dataframe
-df = pd.DataFrame(rows, columns=columns)
-# результат
-print(df[['title','dbirth','salary','last_work']])
+def get_resume(query,area,period):
+  # колонки и массив строк для dataframe
+  columns = ['title','dbirth','salary','last_work','last_work_per','url']
+  rows = []
+  i=0
+  # запрашивать пока массив не опустеет
+  while len(get_all(query,area,period,i)) > 0:
+    result_all = get_all(query,area,period,i)
+    # i = i + 1
+    i +=1
+    for item in result_all:
+      id = item.find('a', href=True)['href']
+      result_one = get_one(id)
+      soup_one = BeautifulSoup(result_one.content, 'html.parser')
+      title = soup_one.find('span',class_='resume-block__title-text resume-block__title-text_position').find("span")
+      dbirth = soup_one.find('meta',itemprop="birthDate")
+      if dbirth:
+        dbirth = dbirth["content"]
+      else:
+        dbirth = ''    
+      salary = soup_one.find('span',class_='resume-block__salary resume-block__title-text_salary')
+      last_work = soup_one.find('div',class_='resume-block__sub-title')
+      last_work_per = soup_one.find('div',class_='bloko-column bloko-column_xs-4 bloko-column_s-2 bloko-column_m-2 bloko-column_l-2')
+      row = [if_null_bf(title),dbirth,if_null_num(salary),if_null_bf(last_work),if_null_bf(last_work_per),'https://hh.ru'+id]
+      rows.append(row)
 
-
-
-
+  df = pd.DataFrame(rows, columns=columns)
+  # print(df[['title','dbirth','salary','last_work']])
+  # return(df.agg({'salary':['mean','median','count']}))
+  return df
---
+++ start.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+from hh_vacancy import get_vacancy
+from hh_resume import get_resume
+# query = 'python'
+for query in ['Java','nodejs','plsql','python']:
+     
+    area = 1
+    period = 30
+
+    df_vacancy = get_vacancy(query,area)
+    df_resume = get_resume(query,area,period)
+
+    resume_salary = round(df_resume['salary'].mean())
+    resume_count = df_resume['salary'].count()
+    vacancy_salary = round(df_vacancy['salary.from'].mean())
+    vacancy_count = df_vacancy['salary.from'].count()
+    salary_balance = (resume_salary  + vacancy_salary) / 2
+
+    print(query + ': спрос: ' + str(vacancy_salary) + '('+ str(vacancy_count) + ')'+ '; предложение :' + str(resume_salary) + '('+ str(resume_count) + ')'+'; равновесная: ' + str(salary_balance))
